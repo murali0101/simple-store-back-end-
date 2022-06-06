362a2325ee4a3f86ccd659fa3b0e2f657dd959e6
murali0101 <muralimv0101@gmail.com>
2022-06-06 21:36:44 +0530

    end point with query

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -3,7 +3,50 @@ const Product = require("../models/product.model");
 const router = express.Router();
 router.get("/", async (req, res) => {
   try {
-    const product = await Product.find().lean().exec();
+    let query = { $and: [{ product_color: { $eq: "Violet" } }] };
+    let page = req.query.page || 1;
+    let perPage = req.query.perPage || 10;
+    let skip = (page - 1) * perPage;
+    let sortData = req.query.sort
+      ? {
+          product_price: `${req.query.sort == "asc" ? 1 : -1}`,
+        }
+      : null;
+    // console.log(sortData, req.query.sort);
+    let totalPage = Math.ceil(
+      (await Product.find().countDocuments()) / perPage
+    );
+    const product = await Product.find(query)
+      .skip(skip)
+      .limit(perPage)
+      .sort(sortData);
+
+    return res.status(200).send({ product, totalPage });
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+router.post("/", async (req, res) => {
+  try {
+    const product = await Product.create();
+    return res.status(200).send(product);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+router.patch("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    return res.status(200).send(product);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+router.delete("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
     return res.status(200).send(product);
   } catch (error) {
     return res.status(400).send(error.message);
